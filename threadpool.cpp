25adfada58738dc0e2a0f60447e9cb52ca163391
Yunzhe Su <39934629+cy2333ytu@users.noreply.github.com>
2024-01-16 10:43:45 +0800

    Update threadpool.cpp
    add test code

--- threadpool.cpp
+++ threadpool.cpp
@@ -1,68 +1,94 @@
-#include <iostream>
-#include <functional>
-#include <thread>
-#include <future>
-#include <chrono>
-using namespace std;
-
 #include "threadpool.h"
+#include <benchmark/benchmark.h>
+#include "threadpool.h"
+#include <iostream>
 
-
-/*
-如何能让线程池提交任务更加方便
-1. pool.submitTask(sum1, 10, 20);
-   pool.submitTask(sum2, 1 ,2, 3);
-   submitTask:可变参模板编程
-
-2. 造了一个Result以及相关的类型
-    C++11 线程库   thread   packaged_task(function函数对象)  async 
-   使用future来代替Result节省线程池代码
-*/
-
-int sum1(int a, int b)
-{
-    this_thread::sleep_for(chrono::seconds(2));
-    // 比较耗时
-    return a + b;
-}
-int sum2(int a, int b, int c)
-{
-    this_thread::sleep_for(chrono::seconds(2));
-    return a + b + c;
+void fun1(int slp) {
+    std::cout << "  hello, fun1 !" << std::this_thread::get_id() << "\n";
+    if (slp > 0) {
+        std::cout << " ======= fun1 sleep " << slp << "=========" << std::this_thread::get_id();
+        std::this_thread::sleep_for(std::chrono::milliseconds(slp));
+    }
 }
 
-int main()
-{
-    ThreadPool pool;
-    // pool.setMode(PoolMode::MODE_CACHED);
-    pool.start(2);
+struct gfun {
+    int operator()(int n) {
+        std::cout << n << " hello, gfun ! " << std::this_thread::get_id();
+        return 42;
+    }
+};
 
-    future<int> r1 = pool.submitTask(sum1, 1, 2);
-    future<int> r2 = pool.submitTask(sum2, 1, 2, 3);
-    future<int> r3 = pool.submitTask([](int b, int e)->int {
-        int sum = 0;
-        for (int i = b; i <= e; i++)
-            sum += i;
-        return sum;
-        }, 1, 100);
-    future<int> r4 = pool.submitTask([](int b, int e)->int {
-        int sum = 0;
-        for (int i = b; i <= e; i++)
-            sum += i;
-        return sum;
-        }, 1, 100);
-    future<int> r5 = pool.submitTask([](int b, int e)->int {
-        int sum = 0;
-        for (int i = b; i <= e; i++)
-            sum += i;
-        return sum;
-        }, 1, 100);
-    //future<int> r4 = pool.submitTask(sum1, 1, 2);
+class A {    // The function must be static to be used in a thread pool
+public:
+    static int Afun(int n = 0) {
+        std::cout << n << "  hello, Afun !  " << std::this_thread::get_id() << std::endl;
+        return n;
+    }
 
-    cout << r1.get() << endl;
-    cout << r2.get() << endl;
-    cout << r3.get() << endl;
-    cout << r4.get() << endl;
-    cout << r5.get() << endl;
+    static std::string Bfun(int n, std::string str, char c) {
+        std::cout << n << "  hello, Bfun !  " << str.c_str() << "  " << (int)c << "  " << std::this_thread::get_id() << std::endl;
+        return str;
+    }
+};
 
+static void BM_ThreadpoolConcurrency(benchmark::State& state) {
+    try {
+        ccy::ThreadPool executor{ 50 };
+        A a;
+        std::future<void> ff = executor.commit(fun1, 0);
+        std::future<int> fg = executor.commit(gfun{}, 0);
+        std::future<int> gg = executor.commit(a.Afun, 9999);
+        std::future<std::string> gh = executor.commit(A::Bfun, 9998, "mult args", 123);
+        std::future<std::string> fh = executor.commit([]()->std::string { std::cout << "hello, fh !  " << std::this_thread::get_id() << std::endl; return "hello,fh ret !"; });
+
+        std::cout << " =======  sleep ========= " << std::this_thread::get_id() << std::endl;
+        std::this_thread::sleep_for(std::chrono::microseconds(900));
+
+        for (int i = 0; i < 50; i++) {
+            executor.commit(fun1, i * 100);
+        }
+        std::cout << " =======  commit all ========= " << std::this_thread::get_id() << " idlsize=" << executor.idlCount() << std::endl;
+
+        std::cout << " =======  sleep ========= " << std::this_thread::get_id() << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+
+        ff.get(); // Calling .get() to retrieve the return value will wait for the thread to finish and get the return value
+        std::cout << fg.get() << "  " << fh.get().c_str() << "  " << std::this_thread::get_id() << std::endl;
+
+        std::cout << " =======  sleep ========= " << std::this_thread::get_id() << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+
+        std::cout << " =======  fun1,55 ========= " << std::this_thread::get_id() << std::endl;
+        executor.commit(fun1, 55).get();    // Calling .get() to retrieve the return value will wait for the thread to finish
+
+        std::cout << "end... " << std::this_thread::get_id() << std::endl;
+
+        ccy::ThreadPool pool(4);
+        std::vector< std::future<int> > results;
+
+        for (int i = 0; i < 8; ++i) {
+            results.emplace_back(
+                pool.commit([i] {
+                    std::cout << "hello " << i << std::endl;
+                    std::this_thread::sleep_for(std::chrono::seconds(1));
+                    std::cout << "world " << i << std::endl;
+                    return i * i;
+                })
+            );
+        }
+        std::cout << " =======  commit all2 ========= " << std::this_thread::get_id() << std::endl;
+
+        for (auto&& result : results)
+            std::cout << result.get() << ' ';
+        std::cout << std::endl;
+    }
+    catch (std::exception& e) {
+        std::cout << "some unhappy happened...  " << std::this_thread::get_id() << e.what() << std::endl;
+    }
 }
+
+// Define the range of the number of threads (e.g., from 1 to 16)
+BENCHMARK(BM_ThreadpoolConcurrency)->Range(1, 16);
+
+// Run the benchmark
+BENCHMARK_MAIN();
